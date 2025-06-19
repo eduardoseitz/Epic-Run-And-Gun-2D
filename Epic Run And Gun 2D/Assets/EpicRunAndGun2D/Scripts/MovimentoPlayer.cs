ca18a5a878ccd6f030f87c1896499125f0dc7dff
Eduardo Seitz <eduardoseitz@outlook.com.br>
2025-06-19 19:03:57 -0300

    Implementado movimento de jogador

--- Epic Run And Gun 2D/Assets/EpicRunAndGun2D/Scripts/MovimentoPlayer.cs
+++ Epic Run And Gun 2D/Assets/EpicRunAndGun2D/Scripts/MovimentoPlayer.cs
@@ -1,16 +1,56 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MovimentoPlayer : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Variaveis publicas.
+    public float velocidadeCaminhar = 5f;
+    public float forcaPulo = 7f;
+    public Rigidbody2D rigidbody2D;
+    private bool seEstaNoChao = true;
+    
+    // Variaveis privadas.
+    private float controleHorizontal;
+    
+    // Este metódo é chamado pela Unity no incio do jogo.
+    private void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Este metódo é chamado pela Unity no incio de cada quadro/frame.
+    private void Update()
     {
+        controleHorizontal = Input.GetAxis("Horizontal");
+        Caminhe();
         
+        if(Input.GetButtonDown("Jump"))
+        {
+            Pule();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        seEstaNoChao = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        seEstaNoChao = false;
+    }
+
+    private void Caminhe()
+    {
+        transform.Translate(new Vector2(controleHorizontal * velocidadeCaminhar * Time.deltaTime, 0));
+    }
+
+    private void Pule()
+    {
+        if (rigidbody2D == true && seEstaNoChao == true)
+        {
+            rigidbody2D.AddForceY(forcaPulo, ForceMode2D.Impulse);
+        }
     }
 }
