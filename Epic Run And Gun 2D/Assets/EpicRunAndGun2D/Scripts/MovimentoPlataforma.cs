66a5be5c2fec09cc344736d5f3496918528084a0
Eduardo Seitz <eduardoseitz@outlook.com.br>
2025-06-20 19:02:36 -0300

    Atualizado plataforma

--- Epic Run And Gun 2D/Assets/EpicRunAndGun2D/Scripts/MovimentoPlataforma.cs
+++ Epic Run And Gun 2D/Assets/EpicRunAndGun2D/Scripts/MovimentoPlataforma.cs
@@ -7,35 +7,55 @@ public class MovimentoPlataforma : MonoBehaviour
     public float velocidadeMover = 0.5f;
     
     // Variaveis privadas.
-    private Vector2 posicaoInicial;
-    private Vector2 posicaoFinal;
+    [SerializeField] private Vector2 posicaoInicial;
+    [SerializeField] private Vector2 posicaoFinal;
     [SerializeField] private bool estaIndoProFinal = true;
     
     // Este metódo é chamado pela Unity no incio do jogo.
     void Start()
     {
         posicaoInicial = transform.position;
-        posicaoFinal = new Vector2(0, posicaoInicial.y + Mathf.Abs(moverY));
+        posicaoFinal = new Vector2(0, posicaoInicial.y + moverY);
+        //
+        // if (moverY < 0)
+        // {
+        //     estaIndoProFinal = false;   
+        // }
     }
 
     // Este metódo é chamado pela Unity no incio de cada quadro/frame.
     void Update()
     {
-        if (transform.position.y < posicaoFinal.y && estaIndoProFinal == true)
+        if (estaIndoProFinal == true)
         {
-            transform.Translate(new Vector2(0, Mathf.Abs(velocidadeMover) * Time.deltaTime));
+            if (Vector2.Distance(transform.position, posicaoFinal) > 0.1f)
+            {
+                transform.Translate(new Vector2(0, Mathf.Abs(velocidadeMover) * Mathf.Sign(moverY) * Time.deltaTime));
+            }
+            else
+            {
+                estaIndoProFinal = false;
+            }
         }
-        else if (transform.position.y >= posicaoFinal.y)
+        else if (estaIndoProFinal == false)
         {
-            estaIndoProFinal = false;
+            if (Vector2.Distance(transform.position, posicaoInicial) > 0.1f)
+            {
+                transform.Translate(new Vector2(0, Mathf.Abs(velocidadeMover) * -Mathf.Sign(moverY) * Time.deltaTime));
+            }
+            else
+            {
+                estaIndoProFinal = true;
+            }
         }
-        if (transform.position.y > posicaoInicial.y && estaIndoProFinal == false)
-        {
-            transform.Translate(new Vector2(0, -Mathf.Abs(velocidadeMover) * Time.deltaTime));
-        }
-        else if (transform.position.y <= posicaoInicial.y)
-        {
-            estaIndoProFinal = true;
-        }
+        
+        // if (transform.position.y > posicaoInicial.y && estaIndoProFinal == false)
+        // {
+        //     transform.Translate(new Vector2(0, -Mathf.Abs(velocidadeMover) * Time.deltaTime));
+        // }
+        // else if (transform.position.y <= posicaoInicial.y)
+        // {
+        //     estaIndoProFinal = true;
+        // }
     }
 }
